11e95e9e9539483a2a761d86561684c44138dd4d
meetsanjaychandel <meetsanjaychandel@gmail.com>
2024-04-18 11:04:15 +0530

    apierror utils added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@
 import dotenv from "dotenv"
 dotenv.config({path:'./.env'});
 
-
+import {app} from "./app.js";
 import connectDB from "./db/db.js";
-connectDB();
+
+connectDB()
+.then(()=>{
+    app.on("Error",(error)=>{
+        console.log("MongoDb connection failed ",error);
+        throw error;
+    });
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server running on port : ${process.env.PORT}`);
+    });
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!",err);
+})
 
 /*
-
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
 import express from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+//1.step- const asyncHandler = ()=>{}
+//2.step- const asyncHandler = (func)=>{()=>{}}
+//2.step- const asynHandler = (fn)=>()=>{}
+// 3.step- const asyncHandler=(func)=>async()=>{}
+
+//below is a async higher order function which is accepting
+//  a function as a parameter.
+
+// const asyncHandler=(func)=>async(req,res,next)=>{
+//     try{
+//         await func(req,res,next)
+//     }
+//     catch(error){
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
+
+const asyncHandler = (requestHandler)=>{(req,res,next)=>{
+     Promise.resolve(requestHandler(req,res,next))
+     .catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler};
